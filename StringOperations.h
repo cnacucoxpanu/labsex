472b4f7e3b68dcc54055d61933c00a8b17ec8dc9
helozer <maxxximderkachev@gmail.com>
2025-09-28 19:42:54 +0300

    1

---
+++ StringOperations.h
@@ -0,0 +1,30 @@
+#define STRINGOPERATIONS_H
+#ifndef STRINGOPERATIONS_H
+
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+
+class StringOperations {
+    private:
+        char *str;
+        int length;
+
+    public: 
+        StringOperations();
+        StringOperations(const char* input);
+        StringOperations(const StringOperations& other);
+        ~StirngOperations();
+
+        StringOpearations& opeator=(const StringOperations& other);
+        StringOpearations operator+(const StringOperations& other) const;
+        StringOperations& operator+=(const StringOpeartions& other);
+        bool operator==(const StringOperations& other) const;
+        bool operator<(const StringOperations& other) const;
+        
+
+}
+
+
+
+#endif
