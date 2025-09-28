a7ef819b002fababe810511cb2aa914cc96a4f77
helozer <maxxximderkachev@gmail.com>
2025-09-28 20:15:12 +0300

    yh

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-// main.cpp
+#include <windows.h>
 #include "MenuManager.h"
 
 int main() {
+    SetConsoleOutputCP(65001); // UTF-8
+    SetConsoleCP(65001); // UTF-8
+    
     MenuManager menu;
     menu.run();
     return 0;
